aea5121cfeb8d97b3b3cecde7e4fcb399af7252d
Unique <dotunique@protonmail.com>
2018-10-24 16:49:29 +0200

    Initial Commit. sockets , Design , Functions

---
+++ Client/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        private const int _BUFFER_SIZE = 20971520;
+        private static int _PORT = 100;
+        private static string _IP = "192.168.0.48";
+        private static TcpClient _client;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                TcpClient _client = new TcpClient(_IP, _PORT);
+                Console.WriteLine("What message?");
+                string message = Console.ReadLine();
+
+                int byteCount = Encoding.ASCII.GetByteCount(message + 1);
+
+                byte[] sendData = new byte[byteCount];
+
+                sendData = Encoding.ASCII.GetBytes(message + ';');
+
+                NetworkStream stream = _client.GetStream();
+
+                stream.Write(sendData, 0, sendData.Length);
+
+                stream.Close();
+                _client.Close();
+            }
+
+
+        }
+    }
+}
---
+++ Server/Server/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        private static int _PORT = 100;
+        public static TcpListener _listener = new TcpListener(IPAddress.Any, _PORT);
+        static TcpClient _client = default(TcpClient);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private static void Server()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+            try
+            {
+                _listener.Start();
+                MessageBox.Show("Server Started");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+
+            while (true)
+            {
+                try
+                {
+                    _client = _listener.AcceptTcpClient();
+                    byte[] receivedBuffer = new byte[1024];
+                    NetworkStream stream = _client.GetStream();
+                    stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+                    string msg = Encoding.ASCII.GetString(receivedBuffer, 0, receivedBuffer.Length);
+                    MessageBox.Show(msg);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+
+                }
+
+            }
+            }).Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Server();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
